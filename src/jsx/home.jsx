66618e8c351d5043fa8aef3c41a78ce5c47a5a8f
Samir Mishra <smir.mishra1551@gmail.com>
2021-05-10 21:02:42 +0545

    navbar added to homepage

--- src/jsx/home.jsx
+++ src/jsx/home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import NavBar from './navbar';
 
 export default function Home(){
     return(
         <div className="home-main-container">
-            
+            <section><NavBar /></section>
         </div>
     );
 }
---
+++ src/jsx/navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../scss/navbar.scss';
+
+export default function NavBar(){
+    return (
+        <div className="navbar-main-container">
+            
+        </div>
+    );
+}
