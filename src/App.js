9e9749ed987d90df6db90643778cb29fbe81b4f1
Samir Mishra <smir.mishra1551@gmail.com>
2021-05-10 20:58:19 +0545

    router configured

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-
+import { BrowserRouter as Router, Switch, Route } from react-router-dom;
+import Home from './jsx/home';
 import './App.scss';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-     
+     <Switch>
+       <Route path="/" exact component={Home} />
+     </Switch>
     </div>
+    </Router>
   );
 }
 
