8728114a19800b374797a77ee6a878b2bca45e31
Samir Mishra <smir.mishra1551@gmail.com>
2021-05-12 22:18:20 +0545

    hover and color added'

--- src/jsx/navbar.jsx
+++ src/jsx/navbar.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import '../scss/navbar.scss';
+import React from "react";
+import "../scss/navbar.scss";
 
-export default function NavBar(){
-    return (
-        <div className="navbar-main-container">
-            <div className="logo-text-div"><div className="h1">Samir Mishra</div></div>
-            <div className="navbar-left">
-                <ul>
-                    <li>service</li>
-                    <li>projects</li>
-                    <li>gallery</li>
-                    <li>contact</li>
-                </ul>
-            </div>
+export default function NavBar() {
+  return (
+    <div className="container">
+      <div className="navbar-main-container">
+        <div className="logo-text-div">
+          <div className="h1">Samir Mishra</div>
         </div>
-    );
+        <div className="navbar-left">
+          <ul>
+            <li>service</li>
+            <li>projects</li>
+            <li>gallery</li>
+            <li>contact</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
