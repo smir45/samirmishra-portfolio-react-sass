29b5fb17c0da3d5f0f0224d7b254c24279feb57f
Samir Mishra <smir.mishra1551@gmail.com>
2021-05-14 22:58:47 +0545

    background color added

--- src/jsx/content-box.jsx
+++ src/jsx/content-box.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../scss/container-box.scss';
 
 
-export default function contentbox(){
+export default function Contentbox(){
     return(
         <div className="container-main-outer">
-            <div className="main-container">
-                
+            <div className="main-container-box">
+
             </div>
         </div>
     );
--- src/jsx/home.jsx
+++ src/jsx/home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from './navbar';
+import ContentBox from './content-box';
 
 export default function Home(){
     return(
         <div className="home-main-container">
             <section><NavBar /></section>
+            <section><ContentBox /></section>
         </div>
     );
 }
