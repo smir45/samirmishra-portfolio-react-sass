f052466633ff971443005fd57a5cd52ddb31c273
Samir Mishra <smir.mishra1551@gmail.com>
2021-05-14 22:53:07 +0545

    container box added

---
+++ src/jsx/content-box.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default function contentbox(){
+    return(
+        <div className="container-main-outer">
+            <div className="main-container">
+                
+            </div>
+        </div>
+    );
+}
