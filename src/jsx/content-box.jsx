00cca39cc9ffc9d90ce04435119b8a0fd88f8477
Samir Mishra <smir.mishra1551@gmail.com>
2021-05-14 23:52:47 +0545

    3d effect added

--- src/jsx/content-box.jsx
+++ src/jsx/content-box.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import '../scss/container-box.scss';
+import React from "react";
+import "../scss/container-box.scss";
 
-
-export default function Contentbox(){
-    return(
-        <div className="container-main-outer">
-            <div className="main-container-box">
-
-            </div>
+export default function Contentbox() {
+  return (
+    <div className="container-main-outer">
+      <div className="main-container-box">
+        <div class="wrapper">
+          <h1 contenteditable data-heading="Samir">
+            Samir
+          </h1>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
