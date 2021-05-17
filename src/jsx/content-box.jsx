d0b34627497570bbd3044b89f2117cbf9eaedefe
Samir Mishra <smir.mishra1551@gmail.com>
2021-05-17 21:58:04 +0545

    normal changes

--- src/jsx/content-box.jsx
+++ src/jsx/content-box.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../scss/container-box.scss";
 
 export default function Contentbox() {
+  
   return (
     <div className="container-main-outer">
       <div className="main-container-box">
