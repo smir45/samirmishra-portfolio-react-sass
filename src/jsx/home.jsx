c61653590a4716ded5bf75aa617f45e3ee71e0cd
Samir Mishra <smir.mishra1551@gmail.com>
2021-05-28 19:56:33 +0545

    nav removed

--- src/jsx/home.jsx
+++ src/jsx/home.jsx
@@ -5,7 +5,7 @@ import ContentBox from './content-box';
 export default function Home(){
     return(
         <div className="home-main-container">
-            <section><NavBar /></section>
+            
             <section><ContentBox /></section>
         </div>
     );
